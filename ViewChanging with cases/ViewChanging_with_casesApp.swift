49236019cb4d95fd11e3c31759c07cd25e4c0ee8
SardorbekSaydamatov <saidalo.saydamatov@gmail.com>
2024-10-14 18:01:58 +0500

    Initial Commit

---
+++ ViewChanging with cases/ViewChanging_with_casesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewChanging_with_casesApp.swift
+//  ViewChanging with cases
+//
+//  Created by Sardorbek Saydamatov on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewChanging_with_casesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
